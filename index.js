65a639154b7bba06cee1373b5e7c815d65f63418
byte <127610702+metaBlocks2003@users.noreply.github.com>
2023-12-26 20:19:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,27 +11,6 @@ var port = 3000
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
-// const questions = 
-//   {
-//     message: 'Type what you want: ',
-//     name: "name",
-//     }
-// inquirer
-//   .prompt(questions)
-//   .then((answers) => {
-//     const userInp = answers.name
-//     var qr_png = qr.image(userInp);
-//     qr_png.pipe(fs.createWriteStream(`public/qr.png`));
-//   })
-//   .catch((error) => {
-//     if (error.isTtyError) {
-//       // Prompt couldn't be rendered in the current environment
-//     } else {
-//       // Something else went wrong
-//     }
-//   });
-
-
 
 app.set('view engine', 'ejs');
 
@@ -43,31 +22,11 @@ app.get('/', (req, res)=>{
 })
 
 app.post("/generate", (req,res)=>{
-//   const questions = 
-//   {
-//     type: 'input',
-//     message: 'Type what you want: ',
-//     name: 'txt',
-//     }
-// inquirer
-//   .prompt(questions)
-//   .then(() => {
     const userInp = req.body["txt"] 
     var qr_png = qr.image(userInp);
     qr_png.pipe(fs.createWriteStream(`public/qr.png`));
     console.log(userInp)
-  // })
-  // .catch((error) => {
-  //   if (error.isTtyError) {
-  //     // Prompt couldn't be rendered in the current environment
-  //   } else {
-  //     // Something else went wrong
-  //   }
-  // });
-
-
-  // const gen = req.body["txt"]
-  // console.log(gen)
+  
 })
 app.get('/Generate', (req, res)=>{
   res.render("gen.ejs")
